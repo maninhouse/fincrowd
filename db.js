c1b59ce5b7328c8a5e73478d03cc791d951500e9
maninhouse <maninhouse@protonmail.com>
2019-12-16 13:14:11 +0800

    crazy

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+/*
 var settings = require('../simple-website-with-nodejs/settings');
 var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
@@ -5,3 +6,47 @@ var Server = require('mongodb').Server;
 
 module.exports = new Db(settings.db, new Server(settings.host,
     Connection.DEFAULT_PORT, {}));
+*/
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb+srv://user1:user1@cluster-first-dneha.gcp.mongodb.net/test?retryWrites=true&w=majority";
+var client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  var collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  //client.close();
+});
+if (client.isConnected()){
+    kitty = {
+        name: 'chu',
+        age: 10
+    };
+    client.db("test").collection("devices").save(kitty);
+    console.log(client.db("test").collection("devices").findOne({age: 10}));
+}
+
+/*
+client.connect(uri,  function(err, db){
+    db.collection('data', function(err, collection){
+        collection.find({}, function(err, rows){
+            for(var index in rows)
+                console.log(rows[index]);
+        });
+    })  
+});
+var db = client.db()
+
+data.insert({
+    name: 'Fred',
+    data:{
+        tel: '000',
+        address: '555'
+    }
+}, function(err, docs){
+    if (err){
+        //insert fail
+        return;
+    }
+
+        //insert success
+});
+*/
